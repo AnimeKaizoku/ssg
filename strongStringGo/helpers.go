e5c0e572f6e2ddfed21324eb590daf94ac19fb87
Aliwoto <aminnimaj@gmail.com>
2021-07-13 16:28:17 +0000

    moved all helper functions to helpers.go
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- strongStringGo/helpers.go
+++ strongStringGo/helpers.go
@@ -5,6 +5,196 @@
 
 package strongStringGo
 
+import (
+	"strings"
+
+	sin "github.com/ALiwoto/StrongStringGo/strongStringGo/strongInterfaces"
+)
+
+// Ss will generate a new StrongString
+// with the specified non-encoded string value.
+func Ss(s string) StrongString {
+	strong := StrongString{}
+	strong._setValue(s)
+	return strong
+}
+
+// SsTrimSpace will trim all whitespaces of the
+// specified string value and then will
+// generate a new StrongString with that value.
+func SsTrimSpace(s string) StrongString {
+	s = strings.TrimSpace(s)
+	strong := StrongString{}
+	strong._setValue(s)
+	return strong
+}
+
+// SsTrim will trim all leading and
+// trailing Unicode code points contained in cutset of the
+// specified string value and then will
+// generate a new StrongString with that value.
+func SsTrim(s, cutset string) StrongString {
+	s = strings.Trim(s, cutset)
+	strong := StrongString{}
+	strong._setValue(s)
+	return strong
+}
+
+// Qss will generate a new QString
+// with the specified non-encoded string value.
+func Qss(s string) sin.QString {
+	return SsPtr(s)
+}
+
+// Sb will generate a new StrongString
+// with the specified non-encoded bytes value.
+func Sb(b []byte) StrongString {
+	return Ss(string(b))
+}
+
+// Sb will generate a new StrongString pointer
+// with the specified non-encoded bytes value.
+func SbPtr(b []byte) *StrongString {
+	return SsPtr(string(b))
+}
+
+// QSb will generate a new QString
+// with the specified non-encoded bytes value.
+func Qsb(b []byte) sin.QString {
+	return SsPtr(string(b))
+}
+
+// SS will generate a new StrongString
+// with the specified non-encoded string value.
+func SsPtr(s string) *StrongString {
+	strong := StrongString{}
+	strong._setValue(s)
+	return &strong
+}
+
+func ToStrSlice(qs []sin.QString) []string {
+	tmp := make([]string, len(qs))
+	for i, current := range qs {
+		tmp[i] = current.GetValue()
+	}
+	return tmp
+}
+
+func ToQSlice(strs []string) []sin.QString {
+	tmp := make([]sin.QString, len(strs))
+	for i, current := range strs {
+		tmp[i] = SsPtr(current)
+	}
+	return tmp
+}
+
+func Split(s string, separator ...string) []string {
+	if len(separator) == BaseIndex {
+		return nil
+	}
+
+	final := s
+	for _, myStr := range separator {
+		final = strings.ReplaceAll(final, myStr, sepStr)
+	}
+
+	return FixSplit(strings.Split(final, sepStr))
+}
+
+func SplitN(s string, n int, separator ...string) []string {
+	if len(separator) == BaseIndex {
+		return nil
+	}
+
+	rep := n - BaseOneIndex
+	final := s
+	done := BaseIndex
+
+	for _, myStr := range separator {
+		if done < rep {
+			if strings.Contains(final, myStr) {
+				final = strings.Replace(final, myStr, sepStr, rep)
+				done++
+			}
+		} else {
+			break
+		}
+
+	}
+
+	theS := strings.SplitN(final, sepStr, n)
+
+	return FixSplit(theS)
+}
+
+func SplitSlice(s string, separator []string) []string {
+	if len(separator) == BaseIndex {
+		return nil
+	}
+
+	final := s
+	for _, myStr := range separator {
+		final = strings.ReplaceAll(final, myStr, sepStr)
+	}
+
+	return FixSplit(strings.Split(final, sepStr))
+}
+
+func SplitSliceN(s string, separator []string, n int) []string {
+	if len(separator) == BaseIndex {
+		return nil
+	}
+
+	rep := n - BaseOneIndex
+	final := s
+	done := BaseIndex
+
+	for _, myStr := range separator {
+		if done < rep {
+			if strings.Contains(final, myStr) {
+				final = strings.Replace(final, myStr, sepStr, rep)
+				done++
+			}
+		} else {
+			break
+		}
+
+	}
+
+	theS := strings.SplitN(final, sepStr, n)
+
+	return FixSplit(theS)
+}
+
+// FixSplit will fix the bullshit bug in the
+// Split function (which is not ignoring the spaces between strings).
+func FixSplit(myStrings []string) []string {
+	final := make([]string, BaseIndex, cap(myStrings))
+
+	for _, current := range myStrings {
+		if !IsEmpty(&current) {
+			final = append(final, current)
+		}
+	}
+
+	return final
+}
+
+// IsEmpty function will check if the passed-by
+// string value is empty or not.
+func IsEmpty(s *string) bool {
+	return len(*s) == BaseIndex
+}
+
+// YesOrNo returns yes if v is true, otherwise no,
+func YesOrNo(v bool) string {
+	if v {
+		return Yes
+	} else {
+		return No
+	}
+}
+
 func repairString(value *string) *string {
 	entered := false
 	ignoreNext := false
